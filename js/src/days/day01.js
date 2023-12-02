fc0f40aab17f2dae9d8258d6e7a5b13260270c5a
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-01 22:32:46 -0700

    solved part 2 of day 1

--- js/src/days/day01.js
+++ js/src/days/day01.js
@@ -20,31 +20,49 @@ const part1 = (input) => {
     let d2 = match2[1]
     return Number(d1 + d2)
   })
-  console.log(numbers)
   return sum(numbers)
 }
 
-// const part2 = (input) => {
-//   const depths = parseInput(input)
-//   const depthWindows = windows(3, depths)
-//   let prev = sum(depthWindows.next().value)
-//   let increases = 0
-//   for (const depthWindow of depthWindows) {
-//     let depthTotal = sum(depthWindow)
-//     if (depthTotal > prev) { increases++ }
-//     prev = depthTotal
-//   }
-//   return increases
-// }
+const firstDigitMatch2 = /(\d|one|two|three|four|five|six|seven|eight|nine)/
+const lastDigitMatch2 = /(\d|one|two|three|four|five|six|seven|eight|nine).*(\d|one|two|three|four|five|six|seven|eight|nine)/
+
+const wordMap = {
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9,
+}
+
+const part2 = (input) => {
+  const lines = parseInput(input)
+  const numbers = lines.map((line) => {
+    const match1 = firstDigitMatch2.exec(line)
+    if (!match1) { return 0 }
+    let match2 = lastDigitMatch2.exec(line)
+    if (!match2) { match2 = {} }
+    let d1 = match1[1]
+    d1 = wordMap[d1] || d1
+    let d2 = match2[2]
+    d2 = wordMap[d2] || d2
+    if (d2 == null) { d2 = d1 }
+    return Number(String(d1) + String(d2))
+  })
+  return sum(numbers)
+}
 
 await runner.testOutput('day01/example', '1', part1)
-await runner.printOutput('day01/test', part1)
-await runner.copyOutput('day01/test', part1)
+// await runner.printOutput('day01/test', part1)
+// await runner.copyOutput('day01/test', part1)
 // await runner.writeOutput('day01/test', '1', part1)
 // await runner.testOutput('day01/test', '1', part1)
 
-// await runner.testOutput('day01/example', '2', part2)
+await runner.testOutput('day01/example', '2', part2)
 // await runner.printOutput('day01/test', part2)
 // await runner.copyOutput('day01/test', part2)
 // await runner.writeOutput('day01/test', '2', part2)
-// await runner.testOutput('day01/test', '2', part2)
+await runner.testOutput('day01/test', '2', part2)
