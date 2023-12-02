916bcee731730e009f23462bab2c119adf2a95ba
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-01 22:15:33 -0700

    solve part 1

--- js/src/days/day01.js
+++ js/src/days/day01.js
@@ -4,40 +4,46 @@ import {windows, sum} from '../lib/iterators.js'
 console.log("Solving AoC 2023 day 1")
 
 const parseInput = (input) => {
-  return input.split("\n").map(n => Number.parseInt(n))
+  return input.split("\n")
 }
+
+const firstDigitMatch = /^[^\d]*(\d)/
+const lastDigitMatch = /(\d)[^\d]*$/
 
 const part1 = (input) => {
-  const depths = parseInput(input).values()
-  let increases = 0
-  let prev = depths.next().value
-  for (const depth of depths) {
-    if (depth > prev) { increases++ }
-    prev = depth
-  }
-  return increases
+  const lines = parseInput(input)
+  const numbers = lines.map((line) => {
+    const match1 = firstDigitMatch.exec(line)
+    if (!match1) { return 0 }
+    const match2 = lastDigitMatch.exec(line)
+    const d1 = match1[1]
+    let d2 = match2[1]
+    return Number(d1 + d2)
+  })
+  console.log(numbers)
+  return sum(numbers)
 }
 
-const part2 = (input) => {
-  const depths = parseInput(input)
-  const depthWindows = windows(3, depths)
-  let prev = sum(depthWindows.next().value)
-  let increases = 0
-  for (const depthWindow of depthWindows) {
-    let depthTotal = sum(depthWindow)
-    if (depthTotal > prev) { increases++ }
-    prev = depthTotal
-  }
-  return increases
-}
+// const part2 = (input) => {
+//   const depths = parseInput(input)
+//   const depthWindows = windows(3, depths)
+//   let prev = sum(depthWindows.next().value)
+//   let increases = 0
+//   for (const depthWindow of depthWindows) {
+//     let depthTotal = sum(depthWindow)
+//     if (depthTotal > prev) { increases++ }
+//     prev = depthTotal
+//   }
+//   return increases
+// }
 
 await runner.testOutput('day01/example', '1', part1)
-// await runner.printOutput('day01/test', part1)
-// await runner.copyOutput('day01/test', part1)
+await runner.printOutput('day01/test', part1)
+await runner.copyOutput('day01/test', part1)
 // await runner.writeOutput('day01/test', '1', part1)
 // await runner.testOutput('day01/test', '1', part1)
 
-await runner.testOutput('day01/example', '2', part2)
+// await runner.testOutput('day01/example', '2', part2)
 // await runner.printOutput('day01/test', part2)
 // await runner.copyOutput('day01/test', part2)
 // await runner.writeOutput('day01/test', '2', part2)
