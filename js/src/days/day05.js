f6a6ceece2fc3d4c3806f573801354cf552e8893
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-05 00:03:25 -0700

    handle 3 of 4 cases for part 2

--- js/src/days/day05.js
+++ js/src/days/day05.js
@@ -45,7 +45,7 @@ const parseInput = (input) => {
   let hasMoreLines = true
   while (hasMoreLines) {
     const [item1, item2, ranges, hasMore] = readNextMapping(linesIterator)
-    mappings.set(item1, {to: item2, ranges})
+    mappings.set(item1, {from: item1, to: item2, ranges})
     hasMoreLines = hasMore
   }
 
@@ -78,9 +78,76 @@ const part1 = (input) => {
   return Math.min.apply(null, currentItem.mappedValues)
 }
 
-// const part2 = (input) => {
-//   return 0
-// }
+const initRangesToMap = (mappedValues) => {
+  const rangesToMap = []
+  for (let i = 0; i < mappedValues.length; i += 2) {
+    rangesToMap.push([mappedValues[i], mappedValues[i + 1]])
+  }
+  return rangesToMap
+}
+
+let sawLoop = false
+const part2 = (input) => {
+  const mappings = parseInput(input)
+  inspect(mappings)
+
+  let currentItem = mappings.get("initial-seeds")
+  let currentMapping = mappings.get(currentItem.to)
+  while (currentMapping) {
+    const {to, ranges} = currentMapping
+    console.log("rangestomap")
+    const nextMappedValues = []
+    const rangesToMap = initRangesToMap(currentItem.mappedValues)
+    let nextRangeToMap = rangesToMap.pop()
+    let foundRange = false
+    while (nextRangeToMap) {
+      const [srcStart, srcRange] = nextRangeToMap
+      const srcEnd = srcStart + srcRange
+      for (const [mappedDstStart, mappedSrcStart, mappedRange] of ranges) {
+        const mappedSrcEnd = mappedSrcStart + mappedRange
+        // case 1, src fits entirely within dst
+        if (srcStart >= mappedSrcStart && srcEnd <= mappedSrcEnd) {
+          const offset = srcStart - mappedSrcStart
+          nextMappedValues.push(mappedDstStart + offset, srcRange)
+          foundRange = true
+          break
+        }
+        // case 2, src overlaps start of dst range
+        if (srcStart < mappedDstStart && srcEnd >= mappedDstStart && srcEnd <= mappedSrcEnd) {
+          const remainingLength = mappedDstStart - srcStart - 1
+          rangesToMap.push([srcStart, remainingLength])
+          const mappedLength = srcEnd - mappedDstStart
+          nextMappedValues.push(mappedDstStart, mappedLength)
+          foundRange = true
+          break
+        }
+        // // case 3, src overlaps end of dst range
+        if (srcStart >= mappedDstStart && srcStart <= mappedSrcEnd && srcEnd > mappedSrcEnd) {
+          const remainingLength = srcEnd - mappedSrcEnd - 1
+          rangesToMap.push([mappedSrcEnd + 1, remainingLength])
+          const mappedLength = mappedSrcEnd - srcStart
+          nextMappedValues.push(srcStart, mappedLength)
+          foundRange = true
+          break
+        }
+        // case 1, src fits entirely within dst
+        if (srcStart >= mappedSrcStart && srcEnd <= mappedSrcEnd) {
+        }
+      }
+      // final case: no matches found for any dst range
+      if (!foundRange) {
+        nextMappedValues.push(srcStart, srcRange)
+      }
+      nextRangeToMap = rangesToMap.pop()
+    }
+
+    currentItem = currentMapping
+    currentItem.mappedValues = nextMappedValues
+    currentMapping = mappings.get(to)
+  }
+  inspect(mappings)
+  return 0
+}
 
 await runner.testOutput('day05/example', '1', part1)
 // await runner.printOutput('day05/test', part1)
@@ -88,7 +155,7 @@ await runner.testOutput('day05/example', '1', part1)
 // await runner.writeOutput('day05/test', '1', part1)
 await runner.testOutput('day05/test', '1', part1)
 
-// await runner.testOutput('day05/example', '2', part2)
+await runner.testOutput('day05/example', '2', part2)
 // await runner.printOutput('day05/test', part2)
 // await runner.copyOutput('day05/test', part2)
 // await runner.writeOutput('day05/test', '2', part2)
