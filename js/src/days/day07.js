e31bda890300452da730562ad528b3bcb5bbaf5d
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-06 23:11:05 -0700

    complete day 7 part 2

--- js/src/days/day07.js
+++ js/src/days/day07.js
@@ -3,12 +3,6 @@ import * as runner from "../lib/runner.js"
 
 console.log("Solving AoC 2023 day 07")
 
-const cardStrengthOrder = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-const cardStrength = cardStrengthOrder.reduce((acc, cardChar, i) => {
-  acc[cardChar] = i
-  return acc
-}, {})
-
 const evaluateHandType = (cardCounts) => {
   const maxCount = Math.max.apply(null, [...cardCounts.values()])
   // 5 of a kind
@@ -54,12 +48,12 @@ const parseInput = (input) => {
   })
 }
 
-const compareHandStrengths = (hand1, hand2) => {
+const compareHandStrengths0 = (cardStrengths) => (hand1, hand2) => {
   const typeDifference = hand1.handType - hand2.handType
   if (typeDifference !== 0) { return typeDifference }
   for (let i = 0; i < hand1.handString.length; i++) {
-    const card1Strength = cardStrength[hand1.handString[i]]
-    const card2Strength = cardStrength[hand2.handString[i]]
+    const card1Strength = cardStrengths[hand1.handString[i]]
+    const card2Strength = cardStrengths[hand2.handString[i]]
     if (card1Strength > card2Strength) { return 1 }
     if (card1Strength < card2Strength) { return -1 }
   }
@@ -67,9 +61,17 @@ const compareHandStrengths = (hand1, hand2) => {
   return 0
 }
 
+const cardStrengthsOrderPart1 = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+const cardStrengthsPart1 = cardStrengthsOrderPart1.reduce((acc, cardChar, i) => {
+  acc[cardChar] = i
+  return acc
+}, {})
+
+const compareHandStrengthsPart1 = compareHandStrengths0(cardStrengthsPart1)
+
 const part1 = (input) => {
   const hands = parseInput(input)
-  const sortedHands = hands.toSorted(compareHandStrengths)
+  const sortedHands = hands.toSorted(compareHandStrengthsPart1)
   const winnings = sortedHands.map((hand, rankIndex) => {
     const rank = rankIndex + 1
     return hand.bid * rank
@@ -77,9 +79,46 @@ const part1 = (input) => {
   return sum(winnings)
 }
 
-// const part2 = (input) => {
-//   return 0
-// }
+const cardStrengthsOrderPart2 = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
+const cardStrengthsPart2 = cardStrengthsOrderPart2.reduce((acc, cardChar, i) => {
+  acc[cardChar] = i
+  return acc
+}, {})
+
+const compareHandStrengthsPart2 = compareHandStrengths0(cardStrengthsPart2)
+
+// mutates hand cardCounts and cardType in place
+const applyWildCards = (hand) => {
+  const jokerCount = hand.cardCounts.get("J") || 0
+  if (jokerCount === 0) { return }
+
+  const newCounts = new Map()
+  let maxCardCount = 0
+  let maxCardChar = ""
+  for (const [cardChar, cardCount] of hand.cardCounts) {
+    if (cardChar === "J") { continue }
+    newCounts.set(cardChar, cardCount)
+    if (cardCount > maxCardCount) {
+      maxCardCount = cardCount
+      maxCardChar = cardChar
+    }
+  }
+  newCounts.set(maxCardChar, maxCardCount + jokerCount) // add the joker count to whichever card currently has the highest count
+
+  hand.cardCounts = newCounts
+  hand.handType = evaluateHandType(newCounts)
+}
+
+const part2 = (input) => {
+  const hands = parseInput(input)
+  hands.forEach(applyWildCards)
+  const sortedHands = hands.toSorted(compareHandStrengthsPart2)
+  const winnings = sortedHands.map((hand, rankIndex) => {
+    const rank = rankIndex + 1
+    return hand.bid * rank
+  })
+  return sum(winnings)
+}
 
 await runner.testOutput('day07/example', '1', part1)
 // await runner.printOutput('day07/test', part1)
@@ -87,8 +126,8 @@ await runner.testOutput('day07/example', '1', part1)
 // await runner.writeOutput('day07/test', '1', part1)
 await runner.testOutput('day07/test', '1', part1)
 
-// await runner.testOutput('day07/example', '2', part2)
+await runner.testOutput('day07/example', '2', part2)
 // await runner.printOutput('day07/test', part2)
 // await runner.copyOutput('day07/test', part2)
 // await runner.writeOutput('day07/test', '2', part2)
-// await runner.testOutput('day07/test', '2', part2)
+await runner.testOutput('day07/test', '2', part2)
