18143ae25e13d701830a964c0bba10bc7745c664
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-06 20:40:55 -0700

    parsing all numbers and symbols

--- swift/Sources/days/day03.swift
+++ swift/Sources/days/day03.swift
@@ -1,13 +1,20 @@
-private struct PartNumber {
-    let value = 0
-    let start = 0
-    let end = 0
+private class PartNumber {
+    let value: Int
+    let start: Int
+    let end: Int
     var nearSymbol = false
+
+    init(value: Int, start: Int, end: Int, nearSymbol: Bool) {
+        self.value = value
+        self.start = start
+        self.end = end
+        self.nearSymbol = nearSymbol
+    }
 }
 
 private struct Symbol {
-    let value = ""
-    let column = 0
+    let value: Character
+    let column: Int
 }
 
 private struct Row {
@@ -15,8 +22,63 @@ private struct Row {
     let symbols: [Symbol]
 }
 
+private func isSymbol(char: Character) -> Bool {
+    return char != "." && !char.isNumber
+}
+
+private func parseInput(_ input: String) -> [Row] {
+    let lines = input.split(separator: "\n")
+    return lines.map { line in
+        var numbers: [PartNumber] = []
+        var symbols: [Symbol] = []
+        var numberStart = -1
+        var numberChars = ""
+
+        func handleDigit(char: Character, index: Int) {
+            if numberStart < 0 {
+                numberStart = index
+            }
+            numberChars.append(char)
+        }
+
+        func handleNonDigit(index: Int) {
+            if numberStart < 0 {
+                return
+            }
+            let numberValue = Int(numberChars)!
+            let newNumber = PartNumber(value: numberValue, start: numberStart, end: index - 1, nearSymbol: false)
+            numbers.append(newNumber)
+            numberStart = -1
+            numberChars = ""
+        }
+
+        func handleSymbol(char: Character, index: Int) {
+            let newSymbol = Symbol(value: char, column: index)
+            symbols.append(newSymbol)
+        }
+
+        line.enumerated().forEach { i, char in
+            if char.isNumber {
+                handleDigit(char: char, index: i)
+                return
+            } else {
+                handleNonDigit(index: i)
+            }
+            if isSymbol(char: char) {
+                handleSymbol(char: char, index: i)
+            }
+        }
+
+        handleNonDigit(index: line.count) // treat end of line as a non digit
+
+        return Row(numbers: numbers, symbols: symbols)
+    }
+}
+
 private func part1(input: String) throws -> String {
-    return "0"
+    let rows = parseInput(input)
+    let result = rows.count
+    return String(result)
 }
 
 
