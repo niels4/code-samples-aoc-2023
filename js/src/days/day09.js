166089ec7cd3371f3980a2b5218db502814e83cb
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-09 11:39:23 -0700

    solve day 9 part 1

---
+++ js/src/days/day09.js
@@ -0,0 +1,64 @@
+import { sum, windows } from "../lib/iterators.js"
+import { readNumberList } from "../lib/parsing.js"
+import * as runner from "../lib/runner.js"
+
+console.log("Solving AoC 2023 day 09")
+
+const parseInput = (input) => input.split('\n').map(readNumberList)
+
+const getRowDiffs = (row) => {
+  const diffs = []
+  for (const [l, r] of windows(2, row)) {
+    diffs.push(r - l)
+  }
+  return diffs
+}
+
+const isAllZeros = (array) => array.every(v => v === 0)
+
+const getRowDiffsUntilAllZeros = (row) => {
+  const allDiffs = []
+  let currentRow = row
+  allDiffs.push(currentRow)
+  while (!isAllZeros(currentRow)) {
+    currentRow = getRowDiffs(currentRow)
+    allDiffs.push(currentRow)
+  }
+  return allDiffs
+}
+
+const getNextSequenceValueFromAllDiffs = (allDiffs) => {
+  if (allDiffs.length < 2) { return 0 }
+  let lastDiffValue = 0
+  for (let diffIndex = allDiffs.length - 2; diffIndex >= 0; diffIndex--) {
+    lastDiffValue = allDiffs[diffIndex].at(-1) + lastDiffValue
+  }
+  return lastDiffValue
+}
+
+const part1 = (input) => {
+  const histories = parseInput(input)
+
+  const nextSequenceValues = histories.map((historyRow) => {
+    const allDiffs = getRowDiffsUntilAllZeros(historyRow)
+    return getNextSequenceValueFromAllDiffs(allDiffs)
+  })
+
+  return sum(nextSequenceValues)
+}
+
+// const part2 = (input) => {
+//   return 0
+// }
+
+await runner.testOutput('day09/example', '1', part1)
+// await runner.printOutput('day09/test', part1)
+// await runner.copyOutput('day09/test', part1)
+// await runner.writeOutput('day09/test', '1', part1)
+await runner.testOutput('day09/test', '1', part1)
+
+// await runner.testOutput('day09/example', '2', part2)
+// await runner.printOutput('day09/test', part2)
+// await runner.copyOutput('day09/test', part2)
+// await runner.writeOutput('day09/test', '2', part2)
+// await runner.testOutput('day09/test', '2', part2)
