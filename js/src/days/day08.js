9ac87c4e3b3b0c63be4b06e742dec7994162ae6d
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-09 23:22:57 -0700

    clean up code day 8

--- js/src/days/day08.js
+++ js/src/days/day08.js
@@ -1,4 +1,3 @@
-import { mult } from "../lib/iterators.js"
 import { findGCD } from "../lib/math.js"
 import * as runner from "../lib/runner.js"
 
@@ -21,14 +20,11 @@ const parseInput = (input) => {
   return {directions, nodes}
 }
 
-// solve part 1 the simple way
-const part1 = (input) => {
-  const {directions, nodes} = parseInput(input)
-
+const findStepsUntilEndTarget = (isEndKey, directions, nodes, startNode) => {
+  let currentNode = startNode
   let numSteps = 0
-  let currentNode = nodes.get(startNodeKey)
 
-  while (currentNode.key !== endNodeKey) {
+  while (!isEndKey(currentNode.key)) {
     const nextDirection = directions[numSteps % directions.length]
     const nextNodeKey = currentNode[nextDirection]
     currentNode = nodes.get(nextNodeKey)
@@ -38,132 +34,46 @@ const part1 = (input) => {
   return numSteps
 }
 
-// for each node, map to the node that you end up at after following 1 loop of directions
-const createFullLoopMapping = (directions, nodes) => {
-  const mapping = new Map()
+const isEndKeyPart1 = key => key === endNodeKey
 
-  for (const node of nodes.values()) {
-    let currentNode = node
-    for (const direction of directions) {
-      currentNode = nodes.get(currentNode[direction])
-    }
-    mapping.set(node, currentNode)
-  }
-
-  return mapping
-}
-
-// find which nodes can lead to the end node if we are on them at the start of the direction loop
-const findPotentialStartingLoopTargets = (isEndKey) => (directions, nodes) => {
-  const potentialStartingLoopTargets = new Map()
-
-  ;[...nodes.values()].forEach((node) => {
-    let numSteps = 1
-    let currentNode = node
-    for (const direction of directions) {
-      currentNode = nodes.get(currentNode[direction])
-      if (isEndKey(currentNode.key)) {
-        potentialStartingLoopTargets.set(node, numSteps)
-        return
-      }
-      numSteps++
-    }
-  })
-
-  return potentialStartingLoopTargets
-}
-const isEndKeyPart1 = key => key === "ZZZ"
-const findPotentialStartingLoopTargetsPart1 = findPotentialStartingLoopTargets(isEndKeyPart1)
-
-// improve performance of part 1 to get an intuition for how to solve part 2
-const part1_perf = (input) => {
+const part1 = (input) => {
   const {directions, nodes} = parseInput(input)
-  const fullLoopMapping = createFullLoopMapping(directions, nodes)
-  const potentialStartingLoopTargets = findPotentialStartingLoopTargetsPart1(directions, nodes)
-  let currentNode = nodes.get(startNodeKey)
-  let numLoops = 0
-  while (!potentialStartingLoopTargets.has(currentNode)) {
-    currentNode = fullLoopMapping.get(currentNode)
-    numLoops++
-  }
-  const stepsSoFar = numLoops * directions.length
-  const stepsToTheEnd = potentialStartingLoopTargets.get(currentNode)
-
-  return stepsSoFar + stepsToTheEnd
+  return findStepsUntilEndTarget(isEndKeyPart1, directions, nodes, nodes.get(startNodeKey))
 }
 
 const getAllStartNodes = (nodes) => {
   const startNodes = []
+
   for (const [nodeKey, node] of nodes) {
     if (nodeKey.endsWith("A")) {
       startNodes.push(node)
     }
   }
+
   return startNodes
 }
 
-const foundAllStartingLoopTargets = (potentialStartingLoopTargets, nodes) => nodes.every(node => potentialStartingLoopTargets.has(node))
-
 const isEndKeyPart2 = key => key.endsWith("Z")
-const findPotentialStartingLoopTargetsPart2 = findPotentialStartingLoopTargets(isEndKeyPart2)
 
 const part2 = (input) => {
   const {directions, nodes} = parseInput(input)
 
-  console.log("PART 2")
-  // inspect(fullLoopMapping)
-  // inspect(potentialStartingLoopTargets)
-
   const startNodes = getAllStartNodes(nodes)
-  const cycleLengths = startNodes.map((node) => {
-    let numSteps = 0
-    let currentNode = node
-    while (!isEndKeyPart2(currentNode.key)) {
-      const nextDirection = directions[numSteps % directions.length]
-      const nextNodeKey = currentNode[nextDirection]
-      currentNode = nodes.get(nextNodeKey)
-      numSteps++
-    }
-    return numSteps
-  })
-
-  cycleLengths.forEach((len) => {
-    console.log("got cycleLength", len, len / directions.length)
-  })
-
+  const cycleLengths = startNodes.map(node => findStepsUntilEndTarget(isEndKeyPart2, directions, nodes, node))
   const cycledGCD = findGCD(cycleLengths)
-  const result = cycleLengths.reduce((acc, len) => {
-    return acc * Math.ceil(len / cycledGCD)
+  const gcdMultiple = cycleLengths.reduce((acc, cycleLength) => {
+    return acc * Math.ceil(cycleLength / cycledGCD)
   }, 1)
 
-  return result * cycledGCD
-
-  // let currentNodes = getAllStartNodes(nodes)
-  // console.log("init loop")
-  // inspect(currentNodes)
-
-  // let numLoops = 1
-  // while (!foundAllStartingLoopTargets(potentialStartingLoopTargets, currentNodes)) {
-  //   currentNodes = currentNodes.map(node => fullLoopMapping.get(node))
-  //   numLoops++
-  // }
-  // const stepsSoFar = numLoops * directions.length
-  // const stepsToTheEnd = potentialStartingLoopTargets.get(currentNodes[0])
-  // console.log("ENDED LOOP", numLoops, stepsSoFar, stepsToTheEnd)
-  // inspect(currentNodes)
-  //
-  // return stepsSoFar + stepsToTheEnd
+  return gcdMultiple * cycledGCD
 }
 
 await runner.testOutput('day08/example', '1', part1)
-await runner.testOutput('day08/example', '1', part1_perf)
 await runner.testOutput('day08/example_b', '1', part1)
-await runner.testOutput('day08/example_b', '1', part1_perf)
 // await runner.printOutput('day08/test', part1)
 // await runner.copyOutput('day08/test', part1)
 // await runner.writeOutput('day08/test', '1', part1)
 await runner.testOutput('day08/test', '1', part1)
-await runner.testOutput('day08/test', '1', part1_perf)
 
 await runner.testOutput('day08/example_c', '2', part2)
 // await runner.printOutput('day08/test', part2)
