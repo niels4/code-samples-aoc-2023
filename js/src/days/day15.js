0ff728710faf27144ef1fef6c5f089076abf6efa
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-15 08:02:01 -0700

    handled all operations

--- js/src/days/day15.js
+++ js/src/days/day15.js
@@ -24,9 +24,65 @@ const part1 = (input) => {
   return sum(hashes)
 }
 
-// const part2 = (input) => {
-//   return 0
-// }
+const sequenceRegex = /([a-z]+)([=-])(\d?)/
+
+const parseSequences = (sequenceString) => {
+  const [, label, operation, focalLengthStr] = sequenceRegex.exec(sequenceString)
+  const focalLength = Number(focalLengthStr)
+  const labelHash = hashString(label)
+  return {label, labelHash, operation, focalLength}
+}
+
+const addLensToBox = (boxes, {label, labelHash, focalLength}) => {
+  if (!boxes[labelHash]) {
+    boxes[labelHash] = {
+      size: 0,
+      lenses: {}
+    }
+  }
+  const box = boxes[labelHash]
+  if (box.lenses[label]) {
+    box.lenses[label].focalLength = focalLength
+    return
+  }
+  box.lenses[label] = {
+    index: box.size,
+    focalLength
+  }
+  box.size++
+}
+
+const removeLenseFromBox = (boxes, {label, labelHash}) => {
+  if (!boxes[labelHash]) { return }
+  const box = boxes[labelHash]
+  if (!box.lenses[label]) { return }
+  const lenseIndex = box.lenses[label].index
+  delete box.lenses[label]
+  for (const lense of Object.values(box.lenses)) {
+    if (lense.index > lenseIndex) {
+      lense.index--
+    }
+  }
+  box.size++
+}
+
+const part2 = (input) => {
+  const sequenceStrings = parseInput(input)
+  const sequences = sequenceStrings.map(parseSequences)
+  inspect(sequences)
+  const boxes = []
+  sequences.forEach((sequence) => {
+    const {operation} = sequence
+    if (operation === "=") {
+      addLensToBox(boxes, sequence)
+    } else {
+      removeLenseFromBox(boxes, sequence)
+    }
+  })
+
+  inspect(boxes)
+  return 0
+}
 
 await runner.testOutput('day15/example', '1', part1)
 // await runner.printOutput('day15/test', part1)
@@ -34,7 +90,7 @@ await runner.testOutput('day15/example', '1', part1)
 // await runner.writeOutput('day15/test', '1', part1)
 await runner.testOutput('day15/test', '1', part1)
 
-// await runner.testOutput('day15/example', '2', part2)
+await runner.testOutput('day15/example', '2', part2)
 // await runner.printOutput('day15/test', part2)
 // await runner.copyOutput('day15/test', part2)
 // await runner.writeOutput('day15/test', '2', part2)
