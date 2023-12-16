01334930e28985fd59c9b37a0fc0be8c71c451ca
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-16 09:04:08 -0700

    solve day 16 part 1

--- js/src/days/day16.js
+++ js/src/days/day16.js
@@ -27,124 +27,127 @@ const horizontalDirections = {
   right: directions.right
 }
 
-const markSquareAsVisited = (squaresVisited, square) => {
-  const key = square.join(":")
-  if (!squaresVisited.has(key)) {
-    squaresVisited.set(key, 1)
+const createSquareKey = (square) => square.join(":")
+const createHistoryKey = (squareKey, directionKey) => `${squareKey}:${directionKey}`
+
+const markSquareAsVisited = (visitCount, pathHistory, square, directionKey) => {
+  const squareKey = createSquareKey(square)
+  const historyKey = createHistoryKey(squareKey, directionKey)
+  pathHistory.add(historyKey)
+  if (!visitCount.has(squareKey)) {
+    visitCount.set(squareKey, 1)
     return
   }
-  squaresVisited.set(key, squaresVisited.get(key) + 1)
+  visitCount.set(squareKey, visitCount.get(squareKey) + 1)
 }
-const createSquareKey = (square) => square.join(":")
 
-const isValidSquare = (rows, squaresVisited, square) => {
+const isValidSquare = (rows, pathHistory, square, directionKey) => {
   if (square[1] < 0 || square[1] >= rows.length ||
     square[0] < 0 || square[0] >= rows[0].length) { return false }
-  const nextSymbol = rows[square[1]][square[0]]
-  const visitCount = squaresVisited.get(createSquareKey(square))
-  if (nextSymbol !== empty && visitCount != null && visitCount > 1) { return false }
+  const historyKey = createHistoryKey(createSquareKey(square), directionKey)
+  if (pathHistory.has(historyKey)) { return false }
   return true
 }
 
-const handleHorizontalMovement = (rows, squaresVisited, nextSquare, nextSymbol, directionKey) => {
+const handleHorizontalMovement = (rows, visitCount, pathHistory, nextSquare, nextSymbol, directionKey) => {
   if (nextSymbol === empty || nextSymbol === horizontalSplitter) {
-    followPath0(rows, squaresVisited, nextSquare, directionKey)
+    followPath0(rows, visitCount, pathHistory, nextSquare, directionKey)
     return
   }
   if (nextSymbol === verticalSplitter) {
     Object.keys(verticalDirections).forEach((nextDirectionKey) => {
-      followPath0(rows, squaresVisited, nextSquare, nextDirectionKey)
+      followPath0(rows, visitCount, pathHistory, nextSquare, nextDirectionKey)
     })
     return
   }
   if (nextSymbol === leftMirror) {
     if (directionKey === "right") {
-      followPath0(rows, squaresVisited, nextSquare, "down")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "down")
     } else {
-      followPath0(rows, squaresVisited, nextSquare, "up")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "up")
     }
     return
   }
   if (nextSymbol === rightMirror) {
     if (directionKey === "right") {
-      followPath0(rows, squaresVisited, nextSquare, "up")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "up")
     } else {
-      followPath0(rows, squaresVisited, nextSquare, "down")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "down")
     }
   }
 }
 
-const handleVerticalMovement = (rows, squaresVisited, nextSquare, nextSymbol, directionKey) => {
+const handleVerticalMovement = (rows, visitCount, pathHistory, nextSquare, nextSymbol, directionKey) => {
   if (nextSymbol === empty || nextSymbol === verticalSplitter) {
-    followPath0(rows, squaresVisited, nextSquare, directionKey)
+    followPath0(rows, visitCount, pathHistory, nextSquare, directionKey)
     return
   }
   if (nextSymbol === horizontalSplitter) {
     Object.keys(horizontalDirections).forEach((nextDirectionKey) => {
-      followPath0(rows, squaresVisited, nextSquare, nextDirectionKey)
+      followPath0(rows, visitCount, pathHistory, nextSquare, nextDirectionKey)
     })
     return
   }
   if (nextSymbol === leftMirror) {
     if (directionKey === "down") {
-      followPath0(rows, squaresVisited, nextSquare, "right")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "right")
     } else {
-      followPath0(rows, squaresVisited, nextSquare, "left")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "left")
     }
     return
   }
   if (nextSymbol === rightMirror) {
     if (directionKey === "down") {
-      followPath0(rows, squaresVisited, nextSquare, "left")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "left")
     } else {
-      followPath0(rows, squaresVisited, nextSquare, "right")
+      followPath0(rows, visitCount, pathHistory, nextSquare, "right")
     }
   }
 }
 
-const printVisited = (rows, squaresVisited) => {
+const printVisited = (rows, visitCount) => {
   console.log()
   const visitedRows = rows.map((row, rowIndex) => {
     return [...row].map((char, colIndex) => {
-      return squaresVisited.has(`${colIndex}:${rowIndex}`) ? "#" : char
+      return visitCount.has(`${colIndex}:${rowIndex}`) ? "#" : char
     }).join('')
   })
   console.log(visitedRows.join("\n"))
   console.log()
 }
 
-let followCount = 0
-const followPath0 = (rows, squaresVisited, currentSquare, directionKey) => {
-  followCount++
-  if (followCount > 89) {
-    process.exit(98)
-  }
-  printVisited(rows, squaresVisited)
-  console.log("size", squaresVisited.size)
+let loopCount = 0
+const followPath0 = (rows, visitCount, pathHistory, currentSquare, directionKey) => {
+  // printVisited(rows, visitCount)
+  // console.log("size", visitCount.size, )
+  // loopCount++
+  // if (loopCount > 50) { process.exit(98)}
   const direction = directions[directionKey]
   const nextSquare = currentSquare.map((coord, i) => coord + direction[i])
-  if (!isValidSquare(rows, squaresVisited, nextSquare)) { return }
-  markSquareAsVisited(squaresVisited, nextSquare)
+  if (!isValidSquare(rows, pathHistory, nextSquare, directionKey)) { return }
+  markSquareAsVisited(visitCount, pathHistory, nextSquare, directionKey)
   const nextSymbol = rows[nextSquare[1]][nextSquare[0]]
   if (horizontalDirections[directionKey]) {
-    handleHorizontalMovement(rows, squaresVisited, nextSquare, nextSymbol, directionKey)
+    handleHorizontalMovement(rows, visitCount, pathHistory, nextSquare, nextSymbol, directionKey)
   } else {
-    handleVerticalMovement(rows, squaresVisited, nextSquare, nextSymbol, directionKey)
+    handleVerticalMovement(rows, visitCount, pathHistory, nextSquare, nextSymbol, directionKey)
   }
 }
 
 const followPath = (rows) => {
-  const squaresVisited = new Map()
-  const startSquare = [0, 0]
-  markSquareAsVisited(squaresVisited, startSquare)
-  followPath0(rows, squaresVisited, startSquare, "right")
-  return squaresVisited
+  const visitCount = new Map()
+  const pathHistory = new Set()
+  const startSquare = [-1, 0]
+  const startDirecton = "right"
+  followPath0(rows, visitCount, pathHistory, startSquare, startDirecton)
+  return visitCount
 }
 
 const part1 = (input) => {
   const rows = parseInput(input)
-  const squaresVisited = followPath(rows)
-  return squaresVisited.size
+  const visitCount = followPath(rows)
+  // printVisited(rows, visitCount)
+  return visitCount.size
 }
 
 // const part2 = (input) => {
@@ -155,7 +158,7 @@ await runner.testOutput('day16/example', '1', part1)
 // await runner.printOutput('day16/test', part1)
 // await runner.copyOutput('day16/test', part1)
 // await runner.writeOutput('day16/test', '1', part1)
-// await runner.testOutput('day16/test', '1', part1)
+await runner.testOutput('day16/test', '1', part1)
 
 // await runner.testOutput('day16/example', '2', part2)
 // await runner.printOutput('day16/test', part2)
