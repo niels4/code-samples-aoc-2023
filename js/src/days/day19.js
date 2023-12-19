4802e7d2afbd0a0298732801fe5908c8187eb3af
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-19 10:40:46 -0700

    parsing input

--- js/src/days/day19.js
+++ js/src/days/day19.js
@@ -4,19 +4,48 @@ console.log("Solving AoC 2023 day 19")
 
 const parseInput = (input) => {
   const [workflowLines, ratingLines] = input.split('\n\n')
-  const workflows = workflowLines.split("\n").map((line) => {
+  const workflows = new Map()
+  workflowLines.split("\n").forEach((line) => {
     const rulesStart = line.indexOf('{')
     const name = line.substring(0, rulesStart)
     const rulesLine = line.substring(rulesStart + 1, line.length - 1)
-    console.log("parsing rules", name, rulesLine)
-    return {name}
+    const rules = rulesLine.split(",").map((ruleStr) => {
+      const valueSeparatorIndex = ruleStr.indexOf(":")
+      if (valueSeparatorIndex < 0) {
+        return {nextState: ruleStr, prop: null}
+      }
+
+      const prop = ruleStr[0]
+      const compare = ruleStr[1]
+      const value = Number(ruleStr.substring(2, valueSeparatorIndex))
+      const nextState = ruleStr.substring(valueSeparatorIndex + 1, ruleStr.length)
+      return {prop, compare, value, nextState}
+    })
+
+    workflows.set(name, rules)
   })
-  return {workflows}
+
+  const ratings = ratingLines.split("\n").map((line) => {
+    const propValuesPairs = line.substring(1, line.length - 1).split(",")
+    const rating = {}
+    propValuesPairs.forEach((propValuePair) => {
+      const [prop, value] = propValuePair.split("=")
+      rating[prop] = Number(value)
+    })
+    return rating
+  })
+
+  return {workflows, ratings}
+}
+
+const isPartAcceptable = (rating) => {
+  
 }
 
 const part1 = (input) => {
-  const {workflows} = parseInput(input)
+  const {workflows, ratings} = parseInput(input)
   inspect(workflows)
+  console.log("ratings", ratings)
   return 0
 }
 
