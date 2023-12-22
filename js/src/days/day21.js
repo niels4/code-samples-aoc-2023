8e17a59d8073c61815457a6b6b0bb5120b8293ce
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-21 23:15:28 -0700

    handle wrapping the grid on each step

--- js/src/days/day21.js
+++ js/src/days/day21.js
@@ -32,7 +32,7 @@ const parseInput = (input) => {
     }
   }
 
-  return {startPosition, rockPositions}
+  return {startPosition, rockPositions, numCols, numRows}
 }
 
 const nextPositions = [
@@ -42,13 +42,19 @@ const nextPositions = [
   {x: 1, y: 0}, // right
 ]
 
-const takeStep = (rockPositions, currentPositions) => {
+const takeStep = (numCols, numRows, rockPositions, currentPositions) => {
   const newPositions = new Map()
   for (const position of currentPositions.values()) {
     for (const offset of nextPositions) {
-      const nextPosition = {x: position.x + offset.x, y: position.y + offset.y}
+      const nextX = position.x + offset.x
+      const nextY = position.y + offset.y
+      let rockX = nextX % numCols
+      if (rockX < 0) { rockX = numCols + rockX}
+      let rockY = nextY % numRows
+      if (rockY < 0) { rockY = numRows + rockY}
+      if (rockPositions.has(createPositionKey({x: rockX, y: rockY}))) { continue }
+      const nextPosition = {x: nextX, y: nextY}
       const nextKey = createPositionKey(nextPosition)
-      if (rockPositions.has(nextKey)) { continue }
       newPositions.set(nextKey, nextPosition)
     }
   }
@@ -56,18 +62,22 @@ const takeStep = (rockPositions, currentPositions) => {
 }
 
 const part1 = (numSteps) => (input) => {
-  const {startPosition, rockPositions} = parseInput(input)
+  const {startPosition, rockPositions, numCols, numRows} = parseInput(input)
 
   let currentPositions = new Map()
   currentPositions.set(createPositionKey(startPosition), startPosition)
   for (let i = 0; i < numSteps; i++) {
-    currentPositions = takeStep(rockPositions, currentPositions)
+    currentPositions = takeStep(numCols, numRows, rockPositions, currentPositions)
   }
 
   return currentPositions.size
 }
 
+// const part2Steps = 26501365
+//
 // const part2 = (input) => {
+//   const {startPosition, rockPositions} = parseInput(input)
+//   console.log("start pos", startPosition)
 //   return 0
 // }
 
@@ -77,7 +87,8 @@ await runner.testOutput('day21/example', '1', part1(6))
 // await runner.writeOutput('day21/test', '1', part1(64))
 await runner.testOutput('day21/test', '1', part1(64))
 
-// await runner.testOutput('day21/example', '2', part2)
+await runner.testOutput('day21/example', '2', part1(10))
+await runner.testOutput('day21/example', '3', part1(50))
 // await runner.printOutput('day21/test', part2)
 // await runner.copyOutput('day21/test', part2)
 // await runner.writeOutput('day21/test', '2', part2)
