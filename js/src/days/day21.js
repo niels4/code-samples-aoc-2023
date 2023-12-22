50d599b40496ddebbcfc6d6fae0fc405e7495bdb
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-21 22:34:37 -0700

    start of parsing day 21

---
+++ js/src/days/day21.js
@@ -0,0 +1,46 @@
+import * as runner from "../lib/runner.js"
+
+console.log("Solving AoC 2023 day 21")
+
+const createPositionKey = p => `${p.x}:${p.y}`
+
+const parseInput = (input) => {
+  const rockPositions = new Set()
+  const numCols = input.indexOf("\n")
+  const numRows = (input.length + 1) / (numCols + 1)
+  const startIndex = input.indexOf("S")
+  const startCol = startIndex % (numCols + 1)
+  const startRow = Math.floor(startIndex / (numCols + 1))
+  const startPosition = {x: startCol, y: startRow}
+  if (startRow !== (numRows - 1) / 2) {
+    throw new Error("Assumption invalid: start position not in center row")
+  }
+  if (startCol !== (numCols - 1) / 2) {
+    throw new Error("Assumption invalid: start position not in center column")
+  }
+
+  return {startPosition, rockPositions, numRows, numCols}
+}
+
+const part1 = (numSteps) => (input) => {
+  const {startPosition, rockPositions, numRows, numCols} = parseInput(input)
+  console.log(`rows:${numRows}, cols:${numCols}, startPos:`, startPosition)
+  return 0
+}
+
+// const part2 = (input) => {
+//   return 0
+// }
+
+await runner.testOutput('day21/example', '1', part1(6))
+console.log()
+// await runner.printOutput('day21/test', part1(64))
+// await runner.copyOutput('day21/test', part1)
+// await runner.writeOutput('day21/test', '1', part1)
+// await runner.testOutput('day21/test', '1', part1)
+
+// await runner.testOutput('day21/example', '2', part2)
+// await runner.printOutput('day21/test', part2)
+// await runner.copyOutput('day21/test', part2)
+// await runner.writeOutput('day21/test', '2', part2)
+// await runner.testOutput('day21/test', '2', part2)
