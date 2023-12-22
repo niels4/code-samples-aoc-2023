5a43142f972f35998ee9daa058be6ee480e472a2
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-21 22:54:55 -0700

    solve day 21 part 1

--- js/src/days/day21.js
+++ js/src/days/day21.js
@@ -2,13 +2,16 @@ import * as runner from "../lib/runner.js"
 
 console.log("Solving AoC 2023 day 21")
 
-const createPositionKey = (x, y) => `${x}:${y}`
+const startChar = "S"
+const rockChar = "#"
+
+const createPositionKey = ({x, y}) => `${x}:${y}`
 
 const parseInput = (input) => {
-  const rockPositions = new Set()
+  const rockPositions = new Map()
   const numCols = input.indexOf("\n")
   const numRows = (input.length + 1) / (numCols + 1)
-  const startIndex = input.indexOf("S")
+  const startIndex = input.indexOf(startChar)
   const startCol = startIndex % (numCols + 1)
   const startRow = Math.floor(startIndex / (numCols + 1))
   const startPosition = {x: startCol, y: startRow}
@@ -22,20 +25,46 @@ const parseInput = (input) => {
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
       const index = y * (numCols + 1) + x
-      if (input[index] === "#") {
-        rockPositions.add(createPositionKey(x, y))
+      if (input[index] === rockChar) {
+        const rockPos = {x, y}
+        rockPositions.set(createPositionKey(rockPos), rockPos)
       }
     }
   }
 
-  return {startPosition, rockPositions, numRows, numCols}
+  return {startPosition, rockPositions}
+}
+
+const nextPositions = [
+  {x: 0, y: -1}, // up
+  {x: 0, y: 1}, // down
+  {x: -1, y: 0}, // left
+  {x: 1, y: 0}, // right
+]
+
+const takeStep = (rockPositions, currentPositions) => {
+  const newPositions = new Map()
+  for (const position of currentPositions.values()) {
+    for (const offset of nextPositions) {
+      const nextPosition = {x: position.x + offset.x, y: position.y + offset.y}
+      const nextKey = createPositionKey(nextPosition)
+      if (rockPositions.has(nextKey)) { continue }
+      newPositions.set(nextKey, nextPosition)
+    }
+  }
+  return newPositions
 }
 
 const part1 = (numSteps) => (input) => {
-  const {startPosition, rockPositions, numRows, numCols} = parseInput(input)
-  console.log(`rows:${numRows}, cols:${numCols}, startPos:`, startPosition)
-  inspect(rockPositions)
-  return 0
+  const {startPosition, rockPositions} = parseInput(input)
+
+  let currentPositions = new Map()
+  currentPositions.set(createPositionKey(startPosition), startPosition)
+  for (let i = 0; i < numSteps; i++) {
+    currentPositions = takeStep(rockPositions, currentPositions)
+  }
+
+  return currentPositions.size
 }
 
 // const part2 = (input) => {
@@ -43,11 +72,10 @@ const part1 = (numSteps) => (input) => {
 // }
 
 await runner.testOutput('day21/example', '1', part1(6))
-console.log()
 // await runner.printOutput('day21/test', part1(64))
-// await runner.copyOutput('day21/test', part1)
-// await runner.writeOutput('day21/test', '1', part1)
-// await runner.testOutput('day21/test', '1', part1)
+// await runner.copyOutput('day21/test', part1(64))
+// await runner.writeOutput('day21/test', '1', part1(64))
+await runner.testOutput('day21/test', '1', part1(64))
 
 // await runner.testOutput('day21/example', '2', part2)
 // await runner.printOutput('day21/test', part2)
