c9a89e1b1b1784e420e97d896e2a9ea5a0fb67db
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-23 09:38:50 -0700

    getting correct answer for example but not test input

--- js/src/days/day22.js
+++ js/src/days/day22.js
@@ -4,7 +4,7 @@ console.log("Solving AoC 2023 day 22")
 
 const parseInput = (input) => {
   const lines = input.split('\n')
-  const bricks = lines.map((line) => {
+  const bricks = lines.map((line, brickId) => {
     const [dim1Str, dim2Str] = line.split("~")
     const [x1, y1, z1] = dim1Str.split(",").map(d => Number(d))
     const [x2, y2, z2] = dim2Str.split(",").map(d => Number(d))
@@ -21,28 +21,79 @@ const parseInput = (input) => {
     if (y1 > y2) { throw new Error("y1 > y2") }
     if (z1 > z2) { throw new Error("z1 > z2") }
 
-    return {x1, x2, y1, y2, z1, z2}
+    return {brickId, x1, x2, y1, y2, z1, z2, collisions: new Set()}
   })
+
   return bricks
 }
 
-const indexBricks = (bricks) => {
-  const bricksByZIndex = new Map()
-  bricks.forEach((brick) => {
-    const {x1, x2, y1, y2, z1, z2} = brick
-    const minZ = Math.min(z1, z2)
-    const maxZ = Math.max(z1, z2)
-  })
-  return bricksByZIndex
+const sortByZ = (l, r) => l.z1 - r.z1
+const bottomZ = 1
+
+const getZSet = (zMap, zIndex) => {
+  let zSet = zMap.get(zIndex)
+  if (!zSet) {
+    zSet = new Set()
+    zMap.set(zIndex, zSet)
+  }
+  return zSet
+}
+
+const willBricksCollide = (b1, b2) => {
+  if (b1.x1 > b2.x2) { return false }
+  if (b1.x2 < b2.x1) { return false }
+  if (b1.y1 > b2.y2) { return false }
+  if (b1.y2 < b2.y1) { return false }
+  return true
+}
+
+const moveBrickDown = (zMap, brick) => {
+  const oldTopZSet = getZSet(zMap, brick.z2)
+  oldTopZSet.delete(brick)
+  brick.z2 -= 1
+  brick.z1 -= 1
+  const newBottomZSet = getZSet(zMap, brick.z1)
+  newBottomZSet.add(brick)
+}
+
+const letBrickFall = (zMap, criticalBricks, brick) => {
+  for (let z = brick.z1; z <= brick.z2; z++) {
+    let zSet = getZSet(zMap, z)
+    zSet.add(brick)
+  }
+  while (brick.z1 !== bottomZ && brick.collisions.size === 0) {
+    const nextZ = brick.z1 - 1
+    const nextBricks = zMap.get(nextZ) || []
+    let nextBrick
+    for (nextBrick of nextBricks) {
+      if (willBricksCollide(brick, nextBrick)) {
+        brick.collisions.add(nextBrick)
+      }
+    }
+
+    if (brick.collisions.size === 1) {
+      criticalBricks.add(nextBrick)
+    } else if (brick.collisions.size === 0) {
+      moveBrickDown(zMap, brick)
+    }
+
+    // get all bricks in the z level below
+    // check for collisions with each brick
+    // add each collision to set
+    // if only 1 collision, add it to the critical bricks set
+  }
 }
 
 const part1 = (input) => {
   const bricks = parseInput(input)
-  console.log("got bricks")
-  console.log(bricks)
-  const indexedBricks = indexBricks(bricks)
-  inspect(indexedBricks)
-  return 0
+  bricks.sort(sortByZ)
+  const zMap = new Map()
+  const criticalBricks = new Set()
+  bricks.forEach((brick) => {
+    letBrickFall(zMap, criticalBricks, brick)
+  })
+  const result = bricks.length - criticalBricks.size
+  return result
 }
 
 // const part2 = (input) => {
@@ -50,10 +101,10 @@ const part1 = (input) => {
 // }
 
 await runner.testOutput('day22/example', '1', part1)
-await runner.printOutput('day22/test', part1)
+// await runner.printOutput('day22/test', part1)
 // await runner.copyOutput('day22/test', part1)
 // await runner.writeOutput('day22/test', '1', part1)
-// await runner.testOutput('day22/test', '1', part1)
+await runner.testOutput('day22/test', '1', part1)
 
 // await runner.testOutput('day22/example', '2', part2)
 // await runner.printOutput('day22/test', part2)
