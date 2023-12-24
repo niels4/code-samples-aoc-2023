8d40dfee514da34ce2507536a171930522028b5a
Niels Nielsen <niels.nielsen@gmail.com>
2023-12-24 11:25:02 -0700

    solve day 24 part 1

--- js/src/days/day24.js
+++ js/src/days/day24.js
@@ -20,38 +20,51 @@ const getSlopeIntercept = ({position, velocity}) => {
   return {slope, intercept}
 }
 
-const getIntersection = (stone1, stone2) => {
-  const si1 = getSlopeIntercept(stone1)
-  const si2 = getSlopeIntercept(stone2)
+const getIntersectionTime = (stone, x) => {
+  return (x - stone.position.x) / stone.velocity.x
+}
+
+const getIntersection = (stoneA, stoneB) => {
+  const si1 = getSlopeIntercept(stoneA)
+  const si2 = getSlopeIntercept(stoneB)
   const m = si2.slope - si1.slope
   const b = si1.intercept - si2.intercept
   const x = b / m
   const y = si1.slope * x + si1.intercept
-  return {x, y}
+  const timeA = getIntersectionTime(stoneA, x)
+  const timeB = getIntersectionTime(stoneB, x)
+  return {x, y, timeA, timeB}
 }
 
-const part1 = (input) => {
+const part1 = (windowMin, windowMax) => (input) => {
   const stones = parseInput(input)
-  inspect(stones)
 
-  const s1 = stones[0]
-  const s2 = stones[1]
+  const intersections = []
+  stones.forEach((stoneA, stoneAIndex) => {
+    for (let stoneBIndex = stoneAIndex + 1; stoneBIndex < stones.length; stoneBIndex++) {
+      const stoneB = stones[stoneBIndex]
+      intersections.push(getIntersection(stoneA, stoneB))
+    }
+  })
 
-  const intersection = getIntersection(s1, s2)
-  console.log("intersection", intersection)
-
-  return 0
+  const futureIntersectionsInWindow = intersections.filter(({x, y, timeA, timeB}) => {
+    return x >= windowMin && x <= windowMax &&
+           y >= windowMin && y <= windowMax &&
+           timeA > 0 && timeB > 0
+  })
+  
+  return futureIntersectionsInWindow.length
 }
 
 // const part2 = (input) => {
 //   return 0
 // }
 
-await runner.testOutput('day24/example', '1', part1)
-// await runner.printOutput('day24/test', part1)
-// await runner.copyOutput('day24/test', part1)
-// await runner.writeOutput('day24/test', '1', part1)
-// await runner.testOutput('day24/test', '1', part1)
+await runner.testOutput('day24/example', '1', part1(7, 27))
+// await runner.printOutput('day24/test', part1(200000000000000, 400000000000000))
+// await runner.copyOutput('day24/test', part1(200000000000000, 400000000000000))
+// await runner.writeOutput('day24/test', '1', part1(200000000000000, 400000000000000))
+await runner.testOutput('day24/test', '1', part1(200000000000000, 400000000000000))
 
 // await runner.testOutput('day24/example', '2', part2)
 // await runner.printOutput('day24/test', part2)
